4b79016b2530cb4aec1c3611afa95e2b4625dfeb
Thibault <thibault@macbookair.home>
2025-03-06 17:50:33 +0100

    routine

--- philo/main.c
+++ philo/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tdausque <tdausque@student.42.fr>          +#+  +:+       +#+        */
+/*   By: thibault <thibault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 19:45:12 by thibault          #+#    #+#             */
-/*   Updated: 2025/03/02 11:36:06 by tdausque         ###   ########.fr       */
+/*   Updated: 2025/03/06 17:18:07 by thibault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,8 @@ int	main(int ac, char **av)
 	philo = (t_philo *)malloc(sizeof(t_philo));
 	if (!philo)
 		return (0);
+	philo->time_to_die = ft_atoi(av[2]);
+	philo->time_to_eat = ft_atoi(av[3]);
+	philo->time_to_sleep = ft_atoi(av[4]);
+	return (0);
 }
--- philo/routine.c
+++ philo/routine.c
@@ -3,13 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tdausque <tdausque@student.42.fr>          +#+  +:+       +#+        */
+/*   By: thibault <thibault@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 12:27:30 by tdausque          #+#    #+#             */
-/*   Updated: 2025/03/06 13:05:37 by tdausque         ###   ########.fr       */
+/*   Updated: 2025/03/06 17:47:04 by thibault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib/philo.h"
 
 // write the 3 routine functions: eat, sleep, think
+// give a t_philo * in arg to know which philo is sleeping, eating or thinking
+void	think(t_philo *philo)
+{
+	print_message("is thinking");
+	usleep(200000);
+}
+
+void	sleep(t_philo *philo)
+{
+	print_message("is_sleeping");
+	usleep(philo->time_to_sleep * 1000);
+}
+
+void	eat(t_philo *philo)
+{
+	pthread_mutex_lock(philo->l_fork);
+	pthread_mutex_lock(philo->r_fork);
+	
+	print_message("is eating");
+	usleep(philo->time_to_eat * 1000);
+
+	pthread_mutex_unlock(philo->l_fork);
+	pthread_mutex_unlock(philo->r_fork);
+}
